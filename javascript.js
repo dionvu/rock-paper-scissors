0bbc53bc40ba2816a6468906749feaa753177d48
Dion Vu <151395310+dionvu@users.noreply.github.com>
2023-12-17 14:40:37 -0800

    finished the game function that runs 5 rounds of rsp. Prints the scores and results of the game.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 game();
 
 function getComputerChoice() {
+    // choice is a number between 0 to 3
     let choice = Math.random() * 3;
 
     if(choice <= 1) return "rock";
@@ -9,30 +10,43 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // Ties if computer choice and player choice equal each other
     if(playerSelection == computerSelection) return "It's a tie!";
     
+    // Compares choice of player with computer 
     if(playerSelection == "rock" && computerSelection == "scissors") return "You win!"; 
     else if(playerSelection == "paper" && computerSelection == "rock") return "You win!";
-    else if(playerSlection == "scissors" && computerSelection == "paper") return "You win!";
+    else if(playerSelection == "scissors" && computerSelection == "paper") return "You win!";
     else return "Computer win!";
 }
 
 function game() {
-    let computerChoice;
-    let scoreComputer;
+    let computerChoice = "";
+    let scoreComputer = "";
 
-    let playerChoice;
-    let scorePlayer;
+    let playerChoice = 0;
+    let scorePlayer = 0;
 
     let outcome;
 
+    // Loops 5 times and increments the score of player and computer when they win a round.
     for(let i = 0; i < 5; i++) {
         computerChoice = getComputerChoice();
         playerChoice = prompt(playerChoice);
         
         playerChoice = playerChoice.toLowerCase();
 
-        console.log(playRound(playerChoice, computerChoice));
-    }
+        outcome = playRound(playerChoice, computerChoice);
+        console.log(outcome);
 
+        if(outcome == "You win!") scorePlayer++;
+        if(outcome == "Computer win!") scoreComputer++;
+
+    }
+    
+    // Compares score of player and computer and determines if the player or computer won overall (or if it was a tie)
+    if(scorePlayer == scoreComputer) console.log("Game ends in a tie! Player Score: " + scorePlayer + " Computer Score: " + scoreComputer);
+    else if(scorePlayer > scoreComputer) console.log("Game ends in player win!. Player Score: " + scorePlayer + " Computer Score: " + scoreComputer);
+    else console.log("Game ends in player win!. Player Score: " + scorePlayer + " Computer Score: " + scoreComputer);
 }
+
