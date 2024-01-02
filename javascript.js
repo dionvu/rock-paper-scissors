a28335db2451816c1f973cc8e377c3f1effa93b4
Dion Vu <151395310+dionvu@users.noreply.github.com>
2024-01-02 10:49:04 -0800

    Cleaned javascript.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
 game();
 
 function getComputerChoice() {
     // choice is a number between 0 to 3
     let choice = Math.random() * 3;
 
-    if(choice <= 1) return "rock";
-    if(choice <= 2) return "paper";
-    if(choice <= 3) return "scissors";
+    if(choice <= 1) return ROCK;
+    if(choice <= 2) return PAPER;
+    return SCISSORS;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -14,22 +18,25 @@ function playRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection) return "It's a tie!";
     
     // Compares choice of player with computer 
-    if(playerSelection == "rock" && computerSelection == "scissors") return "You win!"; 
-    else if(playerSelection == "paper" && computerSelection == "rock") return "You win!";
-    else if(playerSelection == "scissors" && computerSelection == "paper") return "You win!";
+    if(
+        (playerSelection == ROCK && computerSelection == SCISSORS) || 
+        (playerSelection == PAPER && computerSelection == ROCK) || 
+        (playerSelection == SCISSORS && computerSelection == PAPER) 
+    ) return "You win!";
     else return "Computer win!";
 }
 
 function game() {
     let computerChoice = "";
-    let scoreComputer = "";
+    let playerChoice = "";
 
-    let playerChoice = 0;
     let scorePlayer = 0;
-
+    let scoreComputer = 0;
+    
+    // final total
     let outcome;
-
-    // Loops 5 times and increments the score of player and computer when they win a round.
+    
+    // Round
     for(let i = 0; i < 5; i++) {
         computerChoice = getComputerChoice();
         playerChoice = prompt(playerChoice);
@@ -38,14 +45,21 @@ function game() {
 
         outcome = playRound(playerChoice, computerChoice);
         console.log(outcome);
-
-        if(outcome == "You win!") scorePlayer++;
-        if(outcome == "Computer win!") scoreComputer++;
+        
+        checkRound(outcome);
     }
-    checkOutcome(scorePlayer, scoreComputer); 
+    //Final score
+    checkTotal(scorePlayer, scoreComputer); 
 }
 
-function checkOutcome(scorePlayer, scoreComputer) {
+// Returns 0 if tie, 1 if player win, 2 if computer win
+function checkRound(outcome) {
+    if(outcome == "You win!") return 0;
+    if(outcome == "Computer win!") return 1;
+    else return 2; 
+}
+
+function checkTotal(scorePlayer, scoreComputer) {
     if(scorePlayer == scoreComputer) console.log("Game ends in a tie! Player Score: " 
         + scorePlayer + " Computer Score: " + scoreComputer);
     else if(scorePlayer > scoreComputer) console.log("Game ends in player win!. Player Score: " 
